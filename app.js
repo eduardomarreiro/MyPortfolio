762260515d9a5ed4a24953a8e991ba3981eb6bef
eduardomarreiro <eduardo4pc@outlook.com>
2022-05-23 10:06:25 -0300

    First Commit - Switch Between Sections

---
+++ app.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransitions(){
+    //Button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    //Sctions Active
+   //Sctions Active 
+   allSections.addEventListener('click', (e) =>{
+    const id = e.target.dataset.id;
+    if(id){
+        //resmove selected from the other btns
+        sectBtns.forEach((btn) =>{
+            btn.classList.remove('active')
+        })
+        e.target.classList.add('active')
+
+        //hide other sections
+        sections.forEach((section)=>{
+            section.classList.remove('active')
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+})
+}
+
+PageTransitions();
