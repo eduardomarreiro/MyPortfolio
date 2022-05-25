6c93982578ca734f496d1242b10530b23665b95b
eduardomarreiro <eduardo4pc@outlook.com>
2022-05-25 18:00:50 -0300

    Seventh Commit - Light Mode

--- app.js
+++ app.js
@@ -34,6 +34,14 @@ function PageTransitions(){
         element.classList.add('active');
     }
 })
+
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    }) 
+
 }
 
 PageTransitions();
